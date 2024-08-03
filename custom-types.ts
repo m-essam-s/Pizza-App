359ff3ad34253a043d268b5b62c16680629d1031
Mohamed Essam <109111496+m-essam-s@users.noreply.github.com>
2024-08-03 11:46:20 +0300

    Added defining optional Address

--- custom-types.ts
+++ custom-types.ts
@@ -1,17 +1,36 @@
+type Address = {
+    street: string,
+    city: string,
+    country: string,
+}
+
 type Person = {
     name: string,
     age: number,
     isStudent: boolean,
-} 
+    address?: Address, // '?' makes it optional property 
+}
 
 let person_1: Person = {
     name: "Joe",
     age: 20,
     isStudent: true,
+    address: {
+        street: "123 Main",
+        city: "Anytown",
+        country: "USA",
+    }
+
 }
 
 let person_2: Person = {
     name: "Joe",
-    age: 20,
-    isStudent: true,
+    age: 30,
+    isStudent: false,
 }
+
+function displayInfo(person: Person){
+    console.log(`${person.name} lives at ${person.address?.street}`)
+}
+
+displayInfo(person_1)
