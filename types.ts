7d79d5af43e4b8d3746616c8e0a25cefda6ac094
Mohamed Essam <109111496+m-essam-s@users.noreply.github.com>
2024-08-03 09:53:29 +0300

    Added types.ts

---
+++ types.ts
@@ -0,0 +1,7 @@
+let myName: string = "Bob"
+
+// Primitive Data Types: string, number, boolean
+// Challenge: Explicitly type the variables below:
+
+let numberOfWheels: number = 4
+let isStudent: boolean = false
