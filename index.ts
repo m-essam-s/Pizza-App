eb3bbb694a63530d98c095b13ab8f49af4abfce7
Mohamed Essam <109111496+m-essam-s@users.noreply.github.com>
2024-08-03 11:51:44 +0300

    Added order as a custom type

--- index.ts
+++ index.ts
@@ -3,6 +3,12 @@ type Pizza = {
     price: number,
 }
 
+type Order = {
+    id: number, 
+    pizza: Pizza,
+    order?: string,
+}
+
 let menu = [
     { name: "Margherita", price: 8 },
     { name: "Pepperoni", price: 10 },
