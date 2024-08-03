d21a01d938c5e79d6cb833dbcee1322c1d831a29
Mohamed Essam <109111496+m-essam-s@users.noreply.github.com>
2024-08-03 10:05:25 +0300

    Added custom-types.ts

---
+++ custom-types.ts
@@ -0,0 +1,17 @@
+type Person = {
+    name: string,
+    age: number,
+    isStudent: boolean,
+} 
+
+let person_1: Person = {
+    name: "Joe",
+    age: 20,
+    isStudent: true,
+}
+
+let person_2: Person = {
+    name: "Joe",
+    age: 20,
+    isStudent: true,
+}
