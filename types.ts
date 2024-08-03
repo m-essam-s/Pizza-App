0118e2099af51f0717dc24a1c2c0243ee00d4f56
Mohamed Essam <109111496+m-essam-s@users.noreply.github.com>
2024-08-03 11:57:03 +0300

    Array data type

--- types.ts
+++ types.ts
@@ -5,3 +5,9 @@ let myName: string = "Bob"
 
 let numberOfWheels: number = 4
 let isStudent: boolean = false
+
+let ages: number[] = [1, 2, 3 ,4]
+
+let names: string[] = ['mohamed', 'malak', 'ennas']
+
+let isTrue: boolean[] = [true, false]
