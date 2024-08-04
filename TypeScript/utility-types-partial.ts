151f360a859633c8031e03f622cee9bf902cc79c
Mohamed Essam <109111496+m-essam-s@users.noreply.github.com>
2024-08-04 12:43:28 +0300

    Utility Types Partial & Omit

---
+++ TypeScript/utility-types-partial.ts
@@ -0,0 +1,53 @@
+type UserRole = "guest" | "member" | "admin"
+
+type User = {
+    id: number,
+    username: string,
+    role: "member" | "contributor" | "admin",
+}
+
+type UpdatedUser = Partial<User> // This will make all properties of User optional
+
+let nextUserId = 1
+
+const users: User[] = [
+    { id: 1, username: "john_doe", role: "member" },
+    { id: 2, username: "jane_doe", role: "admin" },
+    { id: 3, username: "guest_user", role: "guest" },
+];
+
+
+function fetchUserDetails(username: string): User | undefined {
+    const user = users.find(user => user.username === username)
+    if (!user) {
+        throw new Error(`User with username ${username} not found`)
+    }
+    return user
+}
+
+function updateUser(id: number, updates: UpdatedUser) {
+    const foundUser = users.find(user => user.id === id)
+    if (!foundUser) {
+        console.error("User not found!")
+        return
+    }
+    Object.assign(foundUser, updates)
+}
+
+function addNewUser(newUser: Omit<User, "id">): User {
+    const user: User = { 
+        id: nextUserId++,
+        ...newUser 
+    }
+    return user
+}
+
+
+addNewUser({ username: "joe_schmoe", role: "member" })
+
+console.log(users)
+
+updateUser(1, { username: "new_john_doe" });
+updateUser(4, { role: "contributor" });
+
+console.log(users)
